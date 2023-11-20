0f83390301347b3a4280019c308091a70686d169
allan <allankipchumba76@gmail.com>
2023-11-20 14:43:48 +0300

    fix the css bugs present in the menubtn and video in the index.html file caused by the generality of the tailwind css base class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Package from './components/packages/package';
 
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
+
 export default function App() {
   return (
     <div>
